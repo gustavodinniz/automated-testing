7cd197ea469782e4e040522ef9e54215c5cf1237
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-10-18 20:54:11 -0300

    initial project

---
+++ sw-planet-api/src/test/java/com/gustavodiniz/swplanetapi/SwPlanetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavodiniz.swplanetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPlanetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
