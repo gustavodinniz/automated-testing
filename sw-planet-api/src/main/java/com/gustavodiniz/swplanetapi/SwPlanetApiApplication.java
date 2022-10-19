09be5d8e9d6858f6c72417d1aabf0c842d94a22d
gustavodiniz <gustavo.diniz@tangerino.com.br>
2022-10-18 21:10:57 -0300

    added lombok and initial configuration log

--- sw-planet-api/src/main/java/com/gustavodiniz/swplanetapi/SwPlanetApiApplication.java
+++ sw-planet-api/src/main/java/com/gustavodiniz/swplanetapi/SwPlanetApiApplication.java
@@ -1,13 +1,21 @@
 package com.gustavodiniz.swplanetapi;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class SwPlanetApiApplication {
+public class SwPlanetApiApplication implements ApplicationRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SwPlanetApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SwPlanetApiApplication.class, args);
+    }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("[sw planet api] - started successfully.");
+    }
 }
